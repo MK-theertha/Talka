262f10397accdef5956e3306ab600d1528661f56
MK-theertha <theerthamk1998@gmail.com>
2025-07-03 11:33:24 +0530

    update: add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+// import { AuthContext } from './context/AuthContext.jsx';
 
 const App = () => {
+  // const { authUser } = useContext(AuthContext);
+
   return (
     <div className="bg-[url('/bgImage.svg')] bg-cover bg-no-repeat ">
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </div>
   );
 };
--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,14 +1,18 @@
 import { createContext, useState } from 'react';
 
 // @ts-ignore
+// eslint-disable-next-line react-refresh/only-export-components
 export const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
   const [selectedUser, setSelectedUser] = useState(null);
+  const [users, setUsers] = useState([]);
 
   const value = {
     setSelectedUser,
     selectedUser,
+    users,
+    setUsers,
   };
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
